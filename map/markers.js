eefb6aa256216767aa561e9789e532a7e840daf7
Wendy Nie <wendy90n@gmail.com>
2020-08-23 00:10:06 -0700

    adds text box

--- map/markers.js
+++ map/markers.js
@@ -1,11 +1,11 @@
 "use strict";
 
 var resource = {
-  category: 'FOOD',
-  org_name: 'Bay Area Rescue Mission',
-  resource_name: 'dinner',
-  resource_street: '',
-  resource_notes: '',
+  category: 'CLOTHING',
+  org_name: 'Bay Area Women\'s & Children\'s Center',
+  resource_name: 'clothing',
+  resource_street: '318 Leavenworth St, San Francisco, CA 94102',
+  resource_notes: 'For more than three decades, BAWCC has had one of the largest Free Clothing Closets for women and children in San Francisco.  In addition to giving out women and children\'s clothing, we give out smaller household items such as bedding, kitchen items, books, and even small pieces of furniture.',
   longitude: -122.41414,
   latitude: 37.784106,
 };
@@ -13,13 +13,13 @@ var resource = {
 function initMap() {
   var resourceLatLng = {lat: resource.latitude, lng: resource.longitude};
   var sfLatLng = {lat: 37.7749, lng: -122.4194 };
-  var myLatLng = {lat: -25.363, lng: 131.044 };
-  var toLatLng = {lat: 43.6532, lng: -79.3832 };
-
-//  var map = new google.maps.Map(document.getElementById('map'), {
-//    center: {lat: -122.4194, lng: 37.7749 },
-//    zoom: 4,
-//  });
+  var contentString = `<div class='resource_category'>--- ${resource.resource_name} ---</div>
+                        <div class='resource_org_name'>${resource.org_name}</div>
+                       <div class='resource_street'>${resource.resource_street}</div>
+                       <div class='resource_notes'>${resource.resource_notes}</div>`;
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
   
    var map = new google.maps.Map(document.getElementById("map"),
                                     {
@@ -31,5 +31,8 @@ function initMap() {
     map: map,
     icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
     title: resource.resource_name
+  });
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
   });
 }
