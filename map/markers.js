7d6e2cc06ef936c6cf290c4c1df255ae8621d29a
Wendy Nie <wendy90n@gmail.com>
2020-08-23 01:11:08 -0700

    adds multiple markers

--- map/markers.js
+++ map/markers.js
@@ -1,38 +1,42 @@
 "use strict";
 
-var resource = {
-  category: 'CLOTHING',
-  org_name: 'Bay Area Women\'s & Children\'s Center',
-  resource_name: 'clothing',
-  resource_street: '318 Leavenworth St, San Francisco, CA 94102',
-  resource_notes: 'For more than three decades, BAWCC has had one of the largest Free Clothing Closets for women and children in San Francisco.  In addition to giving out women and children\'s clothing, we give out smaller household items such as bedding, kitchen items, books, and even small pieces of furniture.',
-  longitude: -122.41414,
-  latitude: 37.784106,
-};
+const sfLatLng = {lat: 37.7749, lng: -122.4194 };
+
+var markers = []
+var infoWindows = []
 
 function initMap() {
-  var resourceLatLng = {lat: resource.latitude, lng: resource.longitude};
-  var sfLatLng = {lat: 37.7749, lng: -122.4194 };
-  var contentString = `<div class='resource_category'>--- ${resource.resource_name} ---</div>
-                        <div class='resource_org_name'>${resource.org_name}</div>
-                       <div class='resource_street'>${resource.resource_street}</div>
-                       <div class='resource_notes'>${resource.resource_notes}</div>`;
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
-  
-   var map = new google.maps.Map(document.getElementById("map"),
-                                    {
-                                        center: sfLatLng,
-                                        zoom: 13
-                                    });
-  var marker = new google.maps.Marker({
-    position: resourceLatLng,
-    map: map,
-    icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-    title: resource.resource_name
-  });
-  marker.addListener('click', function() {
-    infowindow.open(map, marker);
-  });
+    var map = new google.maps.Map(document.getElementById("map"),
+                                  {
+    center: sfLatLng,
+    zoom: 13
+    });
+    
+    for (var i = 0; i < resources.length; i++) {
+        var resource = resources[i]
+        var resourceLatLng = {lat: resource.lat, lng: resource.long};
+        
+        var contentString = `<div class='resource_category'>--- ${resource.resource_name} ---</div>
+        <div class='resource_org_name'>${resource.org_name}</div>
+        <div class='resource_street'>${resource.address}</div>
+        <div class='resource_notes'>${resource.notes}</div>`;
+        
+        var infoWindow = new google.maps.InfoWindow({
+        content: contentString
+        });
+        
+        var marker = new google.maps.Marker({
+        position: resourceLatLng,
+        map: map,
+        icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+        title: resource.resource_name
+        });
+        
+        google.maps.event.addListener(marker,'click', (function(marker,contentString,infowWindow){
+            return function() {
+                infowWindow.setContent(contentString);
+                infowWindow.open(map,marker);
+            };
+        })(marker,contentString,infoWindow));
+    }
 }
