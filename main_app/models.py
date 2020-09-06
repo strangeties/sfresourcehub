4d22c80efc4c9d632f5f28d3156d60d14fee9b0d
Wendy Nie <wendy90n@gmail.com>
2020-09-06 11:15:25 -0700

    Makes "category" an enum field

--- main_app/models.py
+++ main_app/models.py
@@ -2,11 +2,39 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 
-
 class Resource(models.Model):
+    # constants for the enum field, category.
+    UNKNOWN = "UNKNOWN"
+    FOOD = "FOOD"
+    CLOTHING = "CLOTHING"
+    ADDICTION_RECOVERY = "ADDICTION_RECOVERY"
+    HYGIENE = "HYGIENE"
+    FINANCIAL_EMPOWERMENT = "FINANCIAL_EMPOWERMENT"
+    SHELTER = "SHELTER"
+    WOMEN_AND_CHILDREN = "WOMEN_AND_CHILDREN"
+    MENTAL_HEALTH_SERVICES = "MENTAL_HEALTH_SERVICES"
+    FINANCIAL_EMPOWERMENT = "FINANCIAL_EMPOWERMENT"
+    MEDICAL_ASSISTANCE = "MEDICAL_ASSISTANCE"
+
+    CATEGORIES = (
+        (UNKNOWN, "Unknown"),
+        (FOOD, "Food"),
+        (CLOTHING, "Clothing"),
+        (ADDICTION_RECOVERY, "Addiction Recovery"),
+        (HYGIENE, "Hygiene"),
+        (FINANCIAL_EMPOWERMENT, "Financial Empowerment"),
+        (SHELTER, "Shelter"),
+        (WOMEN_AND_CHILDREN, "Women and Children"),
+        (MENTAL_HEALTH_SERVICES, "Mental Health Services"),
+        (FINANCIAL_EMPOWERMENT, "Financial Empowerment"),
+        (MEDICAL_ASSISTANCE, "Medical Assistance")
+    )
+
     resource_name = models.CharField(max_length=100)
     org_name = models.CharField(max_length=100, blank=True)
-    category = models.CharField(max_length=100, blank=True)
+    category = models.CharField(max_length=100, 
+                                choices=CATEGORIES,
+                                default=UNKNOWN)
     hours = models.TimeField()
     notes = models.TextField(max_length=250, blank=True)
     street = models.CharField(max_length=100, blank=True)
