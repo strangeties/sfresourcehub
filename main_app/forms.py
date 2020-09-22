a0d663d8d2dca18e886cabd4b0efd4fc7710d846
Wendy Nie <wendy90n@gmail.com>
2020-09-22 01:06:44 -0700

    initial html rendering of opening hours

--- main_app/forms.py
+++ main_app/forms.py
@@ -7,14 +7,12 @@ class USPhoneNumberMultiWidget(forms.MultiWidget):
     """
     A Widget that splits US Phone number input into three  boxes.
     """
-    class Media:
-        js = ('js/us_phone_number_multiwidget.js',)
-
     def __init__(self,attrs=None):
+        self.template_name = 'widgets/phone_number.html'
         widgets = (
-            forms.TextInput(attrs={'size':'3','maxlength':'3', 'class':'phone'}),
-            forms.TextInput(attrs={'size':'3','maxlength':'3', 'class':'phone'}),
-            forms.TextInput(attrs={'size':'4','maxlength':'4', 'class':'phone'}),
+            forms.TextInput(attrs={'size':'3','maxlength':'3', 'class':'phone', 'prefix':'+1 ( '}),
+            forms.TextInput(attrs={'size':'3','maxlength':'3', 'class':'phone', 'prefix':') '}),
+            forms.TextInput(attrs={'size':'4','maxlength':'4', 'class':'phone', 'prefix':' - '}),
         )
         super(USPhoneNumberMultiWidget, self).__init__(widgets, attrs)
 
@@ -27,27 +25,46 @@ class USPhoneNumberMultiWidget(forms.MultiWidget):
         values = super(USPhoneNumberMultiWidget, self).value_from_datadict(data, files, name)
         return u'+1 (%s)-%s-%s' % (values[0], values[1], values[2])
 
+class OpeningHoursMultiWidget(forms.MultiWidget):
+    def __init__(self, attrs=None):
+        print('attrs:'+ attrs['weekday'])
+        self.template_name = 'widgets/opening_hours.html'
+        widgets = (forms.TimeInput(attrs={'class': 'time'}),
+                   forms.TimeInput(attrs={'class': 'time'}),
+                   forms.CheckboxInput(attrs={'class': 'time_enabled'}))
+        super(OpeningHoursMultiWidget, self).__init__(widgets, attrs)
+    
+    def decompress(self, value):
+        if value:
+          return value.split('-')
+        return [None,None,None]
+
+    def value_from_datadict(self, data, files, name):
+        return OpeningHours(False, '00:00', '00:00')
+
 class WeeklyOpeningHoursMultiWidget(forms.MultiWidget):
     def __init__(self, attrs=None):
-        widgets = (forms.TextInput(),
-                   forms.TextInput())
+        print('calling __init__')
+        self.template_name = 'widgets/weekly_opening_hours.html'
+        widgets = (OpeningHoursMultiWidget(attrs={'weekday': 'Monday'}),
+                   OpeningHoursMultiWidget(attrs={'weekday': 'Tuesday'}),
+                   OpeningHoursMultiWidget(attrs={'weekday': 'Wednesday'}),
+                   OpeningHoursMultiWidget(attrs={'weekday': 'Thursday'}),
+                   OpeningHoursMultiWidget(attrs={'weekday': 'Friday'}),
+                   OpeningHoursMultiWidget(attrs={'weekday': 'Saturday'}),
+                   OpeningHoursMultiWidget(attrs={'weekday': 'Sunday'}))
         super(WeeklyOpeningHoursMultiWidget, self).__init__(widgets, attrs)
 
     def decompress(self, value):
+        print('calling decompress')
         if value:
             return value.split('-')
-        return [None,None]
+        return [None,None,None,None,None,None,None]
 
     def value_from_datadict(self, data, files, name):
+        print('calling value_from_datadict')
         values = super(WeeklyOpeningHoursMultiWidget, self).value_from_datadict(data, files, name)
-        arr = [OpeningHours(False, '00:00', '00:00'),
-               OpeningHours(False, '00:00', '00:00'),
-               OpeningHours(False, '00:00', '00:00'),
-               OpeningHours(False, '00:00', '00:00'),
-               OpeningHours(False, '00:00', '00:00'),
-               OpeningHours(False, '00:00', '00:00'),
-               OpeningHours(False, '00:00', '00:00')]
-        return WeeklyOpeningHours(arr)
+        return WeeklyOpeningHours(values)
 
 class ContactForm(forms.Form):
     from_email = forms.EmailField(required=True)
@@ -61,9 +78,6 @@ class AddResourceForm(forms.Form):
     opening_hours = forms.CharField(max_length=512, widget=WeeklyOpeningHoursMultiWidget())
     phone = forms.CharField(widget=USPhoneNumberMultiWidget())
   
-    foo = USPhoneNumberMultiWidget()
-    print (foo.media)
-    
     address = forms.CharField(max_length=100, required=True)
     street_number = forms.CharField(widget=forms.HiddenInput(),
                                     max_length=100, required=False, disabled=False)
