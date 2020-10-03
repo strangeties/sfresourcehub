317ea672ec667240ebc1613eef798a40353e835c
Wendy Nie <wendy90n@gmail.com>
2020-10-03 15:34:25 -0700

    updates opening hours interface

--- main_app/static/js/weekly_opening_hours.js
+++ main_app/static/js/weekly_opening_hours.js
@@ -1,22 +1,29 @@
-function updateEnable() {
-for (i = 0; i < 7; i++) {
-    prefix = 'id_opening_hours_' + i + '_'
-    toggle = document.getElementById(prefix + '0')
-    opening_time = document.getElementById(prefix + '1')
-    closing_time = document.getElementById(prefix + '2')
-    opening_time.disabled = !toggle.checked
-    closing_time.disabled = !toggle.checked
-    opening_time.style.visibility = toggle.checked ? 'visible' : 'hidden'
-    closing_time.style.visibility = toggle.checked ? 'visible' : 'hidden'
-}
-}
+function updateEnable(id) {
+console.log('updateEnable: ')
+console.log(id)
 
-function initializeOpeningHourBehavior() {
-for (i = 0; i < 7; i++) {
-    prefix = 'id_opening_hours_' + i + '_'
-    toggle = document.getElementById(prefix + '0')
-    opening_time = document.getElementById(prefix + '1')
-    closing_time = document.getElementById(prefix + '2')
-    toggle.addEventListener("change", updateEnable)
+weekday = document.getElementById(id)
+
+i = id.slice(-1)
+prefix = 'id_opening_hours_' + i + '_'
+toggle = document.getElementById(prefix + '0')
+opening_hours = document.getElementById('opening_hours_times_' + i)
+opening_time = document.getElementById(prefix + '1')
+closing_time = document.getElementById(prefix + '2')
+
+idx = weekday.innerHTML.indexOf(' closed')
+
+if (idx === -1) {
+  weekday.innerHTML = weekday.innerHTML + ' closed' 
+  toggle.checked = false
+  opening_hours.style.visibility = 'hidden'
+  opening_time.style.visibility = 'hidden'
+  closing_time.style.visibility = 'hidden'
+} else {
+  weekday.innerHTML = weekday.innerHTML.substring(0, idx)
+  toggle.checked = true
+  opening_hours.style.visibility = 'visible'
+  opening_time.style.visibility = 'visible'
+  closing_time.style.visibility = 'visible'
 }
 }
